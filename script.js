f2ae094ce85926f352af92ff592f75ed253e2dc4
Mason Williams <154944752+baconatorr@users.noreply.github.com>
2024-05-16 13:03:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,294 +1,277 @@
-let hint;
-let valid;
-let letters;
-let length;
-let lastClickedTile = null;
-let current = ["Enter a word"];
-let currentIds = [];
-let clicked = [];
-let rowWidth;
+const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const vowels = ['A', 'E', 'I', 'O', 'U']
+const t1 = ['T', 'S', 'R'];
+const t2 = ['N', 'H', 'C']
+const t3 = ['D', 'L','K', 'B'];
+const t4 = ['M', 'W', 'F', 'G'];
+const t5 = ['Y', 'P', 'V'];
+const t6 = ['J', 'X', 'Q', 'Z'];
+
+let vowelCount;
+let chosenLetters = [];
+let letter = "";
+let skips = 3;
+let randomIndex;
+let night = false;
+let currentCheck;
+let isFetching = false;
+
 let wordCount = 0;
-let word0Id;
-let word1Id;
-let word2Id;
-let word3Id;
-let wordsCorrect = [];
+let rowCount = 0;
+let wins = parseInt(localStorage.getItem('wins')) || 0; 
+let matches = parseInt(localStorage.getItem('matches')) || 0; 
+let letterCount = 0;
 
-window.onload = () => {
-    document.querySelector(".letter-display").innerText = current.join('');
-    loadSolution();
-    const day = new Date().getDate();
-    const month = new Date().getMonth() + 1;
-    const year = new Date().getFullYear();
-    let currentDate = new Date(`${month}/${day}/${year}`);
-    let storedDate = localStorage.getItem('storedDate');
-    if(currentDate == storedDate){
-        loadStoredData();
-    } else {
-        localStorage.clear();
-        localStorage.setItem('storedDate', currentDate);
-        word0Id = [];
-        word1Id = [];
-        word2Id = [];
-        word3Id = [];
-        wordCount = [];
-        wordsCorrect = [];
-    }
-};
+if (!localStorage.getItem('visited')) {
 
-function getDate() {
-    const day = new Date().getDate();
-    const month = new Date().getMonth() + 1;
-    const year = new Date().getFullYear();
-    let currentDate = new Date(`${month}/${day}/${year}`);
-    let dateDisplay = document.getElementById('date');
-    dateDisplay.innerText = `${month}/${day}`;
-    let compareDate = new Date("5/16/2024");
-    let diffInTime = currentDate.getTime() - compareDate.getTime();
-    let diffInDays = Math.round(diffInTime / (1000 * 3600 * 24));
-    return diffInDays;
+  openModal('modal')
+  localStorage.setItem('wins', 0);
+  localStorage.setItem('matches', 0);
+  localStorage.setItem('visited', 'true');
+
+  
 }
 
-function loadSolution() {
-    fetch('sols.json')
-    .then(response => response.json())
-    .then(data => {
-        let index = getDate();
-        hint = data[index]["hint"];
-        document.querySelector(".hint").innerText = "Hint: " + hint;
-        valid = data[index]["valid"];
-        letters = data[index]["letters"];
-        length = data[index]["length"];
-        loadGrid();
-    });
-}
+document.addEventListener("DOMContentLoaded", function() {
+    chosenLetters = [];
+    generateList();
+    generateLetter();
+    letterCount = 0;
+    wordCount = 0;
+    rowCount = 0;
+    skips = 3;  
+    count = 15;
+    wins = parseInt(localStorage.getItem('wins')) || 0; 
+    matches = parseInt(localStorage.getItem('matches')) || 0; 
+    loadStats();
+    var squares = document.querySelectorAll(".space");
 
-function loadGrid() {
-    const gridContainer = document.querySelector(".grid");
-    gridContainer.innerHTML = ''; 
-    rowWidth = Math.sqrt(length);
-    let r = document.querySelector(':root');
-    r.style.setProperty('--row-width', rowWidth);
 
-    for (let i = 0; i < length; i++) {
-        let div = document.createElement("div");
-        div.className = "tile";
-        div.id = i;
-        div.setAttribute("data-active", "false");
-        gridContainer.appendChild(div);
-    }
-
-    let grids = document.querySelectorAll(".tile");
-    let i = 0;
-    let j = 0;
-    grids.forEach(function(grid) {
-        if (j == rowWidth) {
-            i++;
-            j = 0;
-        }
-        grid.innerText = letters[i][j];
-        j++;
-
-        grid.addEventListener("click", function() {
-            activateLetter(grid);
+    squares.forEach(function(square) {
+        square.addEventListener("click", function() {
+            var squareId = square.id;
+            addLetter(squareId);
         });
     });
-    loadSave();
-}
 
-function activateLetter(tile) {
-    if (tile.getAttribute("data-active") == "true") {
-        if(tile == lastClickedTile){
-            current.pop();
-            currentIds.pop();
-            current = current.length > 0 ? current : ["Enter a word"]
-            document.querySelector(".letter-display").innerText = current.join('');
-            tile.classList.remove("tile-active");
-            tile.setAttribute("data-active", "false");
-            clicked.pop();
-            lastClickedTile = clicked.length > 0 ? clicked[clicked.length - 1] : null
+    function addLetter(squareId) {
+      if(isFetching){
+        return undefined;
+      }
+        console.log("Square clicked:", squareId);
+        let id = document.getElementById(squareId);
+        let existingP = id.querySelector('p');
+        if(!existingP) {
+            id.classList.add("space-add");
+            let text = document.createElement('p');
+            let letterText = document.createTextNode(letter);
+            text.appendChild(letterText);
+            id.appendChild(text);
+            generateLetter();
+            count--;
+            letterCount++;
+            rowCheck(squareId);
         }
-        return;
     }
-    if(lastClickedTile == null){
-        current = [];
+});
+
+function generateList(){
+    vowelCount = Math.floor(Math.random() * (3) + 6); 
+    console.log(vowelCount)
+    let totalConsonants = 16 - vowelCount;
+    let t1Count = Math.floor(Math.max(1, totalConsonants - 6))
+    let t2Count = Math.floor(Math.max(1, totalConsonants - 6))
+    let t3Count = Math.floor(Math.max(1, totalConsonants - 8))
+    let t4Count = Math.floor(Math.max(1, totalConsonants - 9))
+    let t5Count = Math.floor(Math.max(1, totalConsonants - 10))
+    let t6Count = Math.floor(Math.max(1, totalConsonants - 11))
+    while(t1Count + t2Count + t3Count + t4Count + t5Count + t6Count > totalConsonants){
+        if(t6Count > 0){
+            t6Count--;
+        } else if(t5Count > 0){
+            t5Count--;
+        } else {
+            break;
+        }
     }
-    if (lastClickedTile == null || isNeighbor(lastClickedTile.id, tile.id)) {
-        console.log("click");
-        tile.setAttribute("data-active", "true"); 
-        tile.classList.add("tile-active");
-        clicked.push(tile);
-        current.push(tile.innerText);
-        currentIds.push(tile.id);
-        console.log(currentIds)
-        lastClickedTile = clicked[clicked.length -1];
-        document.querySelector(".letter-display").innerText = current.join('');
-        console.log(current);
+
+    for(let i = 0; i < vowelCount; i++){
+        chosenLetters.push(vowels[(Math.floor(Math.random() * vowels.length))])
+    }
+    for(let i = 0; i < t1Count; i++){
+        chosenLetters.push(t1[(Math.floor(Math.random() * t1.length))])
+    }
+    for(let i = 0; i < t2Count; i++){
+        chosenLetters.push(t2[(Math.floor(Math.random() * t2.length))])
+    }
+    for(let i = 0; i < t3Count; i++){
+        chosenLetters.push(t3[(Math.floor(Math.random() * t3.length))])
+    }
+    for(let i = 0; i < t4Count; i++){
+        chosenLetters.push(t4[(Math.floor(Math.random() * t4.length))])
+    }
+    for(let i = 0; i < t5Count; i++){
+        chosenLetters.push(t5[(Math.floor(Math.random() * t5.length))])
+    }
+    for(let i = 0; i < t6Count; i++){
+        chosenLetters.push(t6[(Math.floor(Math.random() * t6.length))])
+    }
+
+    if(vowelCount == 6){
+        chosenLetters.pop()
+    }
+    console.log(chosenLetters);
+}
+
+function generateLetter() {
+    randomIndex = Math.floor(Math.random() * chosenLetters.length)
+    console.log(randomIndex);
+    letter = chosenLetters[randomIndex];
+    if(letter == undefined){
+        letter = letters[Math.floor(Math.random() * letters.length)]
+    }
+    console.log(letter);
+    displayLetter();
+}
+
+function displayLetter(){
+        const display = document.getElementById("letter");
+        display.innerText = letter;
+        let letterElement = document.querySelector('.letter');
+        letterElement.style.animation = 'none';
+        void letterElement.offsetWidth; 
+        letterElement.style.animation = 'pop-in 0.2s ease'; 
+        chosenLetters.splice(randomIndex, 1)
+}
+
+function skip(){
+    if(skips > 0){
+        generateLetter();
+        skips--;
     }
 }
 
-function isNeighbor(lastId, currentId) {
-    lastId = parseInt(lastId);
-    currentId = parseInt(currentId);
-    const lastRow = Math.floor(lastId / rowWidth);
-    const lastCol = lastId % rowWidth;
-    const currentRow = Math.floor(currentId / rowWidth);
-    const currentCol = currentId % rowWidth;
 
-    const rowDiff = Math.abs(lastRow - currentRow);
-    const colDiff = Math.abs(lastCol - currentCol);
-    let william = rowDiff <= 1 && colDiff <= 1 && (lastId != currentId);
-    if(!william){
-        alert('Out of range');
-    }
-    return william;
-}
-
-function reset(){
-    for(let i = 0; i < currentIds.length; i++){
-        let id = document.getElementById(currentIds[i]);
-        id.classList.remove("tile-active");
-        id.setAttribute("data-active", "false"); 
-    }
-    lastClickedTile = null;
-    current = ["Enter a word"];
-    currentIds = [];
-    document.querySelector(".letter-display").innerText = current.join('');
-}
-
-function submit() {
-    let word = current.join('');
-    word = word.toLowerCase();
-    console.log(word);
-    for (let i = 0; i < valid.length; i++) {
-        if (word == valid[i]) {
-            for (let j = 0; j < currentIds.length; j++) {
-                let id = document.getElementById(currentIds[j]);
-                id.classList.add("tile-correct" + i);
-                id.setAttribute("data-active", "true"); 
+function rowCheck(Id){
+    let row = Id.substring(1, 2); 
+    currentCheck = row;
+    let word = [];
+    for(let i = 1; i <= row; i++){
+        let id = document.getElementById("r" + row + "s" + i);
+        if (id) {
+            let check = id.querySelector('p');
+            if (check != null) {
+                word.push(check.textContent);
+            } else {
+                console.log("RNC");
+                return undefined;
             }
-            wordCount++;
-            wordsCorrect.push(i);
-            celebrateVictory();
-            addAnswer(i);
-            setSave(i);
-            lastClickedTile = null;
-            current = ["Enter a word"];
-            currentIds = [];
-            document.querySelector(".letter-display").innerText = current.join('');
-            return;
         }
     }
-    lastClickedTile = lastClickedTile;
-    alert('Invalid Word');
-    console.log('Invalid word');
+    let pushed = word.join(""); 
+    console.log(pushed + " was pushed");
+   Get(pushed);
 }
 
-function celebrateVictory() {
-    if (wordCount === 4) {
-        alert("Congrats!")
-        const canvas = document.querySelector("body");
-        const jsConfetti = new JSConfetti();
-        jsConfetti.addConfetti();
-    }
+
+function Get(word) {
+    rowCount++;
+    console.log(rowCount);
+    isFetching = true;
+    return new Promise((resolve, reject) => {
+        let url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+        fetch(url)
+            .then(response => {
+                if (response.status === 404) {
+                    animate("wrong");
+                    reject(new Error('Word not found in the dictionary.'));
+                } else if (!response.ok) {
+                    Get(word);
+                    throw new Error('Request failed: ' + response.status);
+                } else {
+                    animate("correct");
+                    resolve();
+                }
+            })
+            .catch(error => {
+    });
+  })
 }
 
-function addAnswer(number){
-    let top = document.querySelector('.answers')
-    let div = document.createElement('div');
-    div.className = "answer-display";
-    div.id = "ans" + number;
-    top.append(div);
-    let text = valid[number].toUpperCase() + " ✅";
-    let display = document.getElementById("ans" + number);
-    display.innerText = text;
-    display.classList.add("answer-correct" + number);
-}
-
-function setSave(num){
-    console.log("saved");
-    switch(num){
-        case 0:
-            word0Id = currentIds;
-            word0Id = JSON.stringify(word0Id);
-            localStorage.setItem('word0', word0Id);
-            break;
-        case 1:
-            word1Id = currentIds;
-            word1Id = JSON.stringify(word1Id);
-            localStorage.setItem('word1', word1Id);
-            break;
-        case 2:
-            word2Id = currentIds;
-            word2Id = JSON.stringify(word2Id);
-            localStorage.setItem('word2', word2Id);
-            break;
-        case 3:
-            word3Id = currentIds;
-            word3Id = JSON.stringify(word3Id);
-            localStorage.setItem('word3', word3Id);
-            break;
-    }
-    localStorage.setItem('wordCount', wordCount);
-    localStorage.setItem('wordsCorrect', JSON.stringify(wordsCorrect));
-}
-
-function loadSave(){
-    wordsCorrect = JSON.parse(localStorage.getItem('wordsCorrect')) || [];
-    if (wordsCorrect) {
-        for (let i = 0; i < wordsCorrect.length; i++) {
-            addAnswer(wordsCorrect[i]);
+function animate(rw){
+  isFetching = false;
+    if(rw == "correct" || rw == "wrong") {
+        for(let i = 1; i <= currentCheck; i++){
+            let id = document.getElementById("r" + currentCheck + "s" + i);
+            if (id) {
+                id.classList.add(rw);
+            } else {
+                console.error("Element with ID 'r" + currentCheck + "s" + i + "' not found.");
+            }
         }
+    } else {
+        console.error("Invalid animation type:", rw);
     }
-    console.log(word0Id);
-    if(word0Id != null){
-        for (let j = 0; j < word0Id.length; j++) {
-            let id = document.getElementById(word0Id[j]);
-            id.classList.add("tile-correct" + 0);
-            id.setAttribute("data-active", "true"); 
-        }
+
+    if(rw == "correct"){
+      wordCount++;
     }
-    if(word1Id != null){
-        for (let j = 0; j < word1Id.length; j++) {
-            let id = document.getElementById(word1Id[j]);
-            id.classList.add("tile-correct" + 1);
-            id.setAttribute("data-active", "true"); 
-        }
-    }
-    if(word2Id != null){
-        for (let j = 0; j < word2Id.length; j++) {
-            let id = document.getElementById(word2Id[j]);
-            id.classList.add("tile-correct" + 2);
-            id.setAttribute("data-active", "true"); 
-        }
-    }
-    if(word3Id != null){
-        for (let j = 0; j < word3Id.length; j++) {
-            let id = document.getElementById(word3Id[j]);
-            id.classList.add("tile-correct" + 3);
-            id.setAttribute("data-active", "true"); 
-        }
-    }
+  winCheck();
 }
 
-function loadStoredData() {
-    wordsCorrect = JSON.parse(localStorage.getItem("wordsCorrect")) || [];
-    word0Id = JSON.parse(localStorage.getItem("word0")) || null;
-    word1Id = JSON.parse(localStorage.getItem("word1")) || null;
-    word2Id = JSON.parse(localStorage.getItem("word2")) || null;
-    word3Id = JSON.parse(localStorage.getItem("word3")) || null;
-    wordCount = parseInt(localStorage.getItem('wordCount'), 10) || 0;
+function winCheck(){
+  if(wordCount == 5){
+    console.log('match complete');
+      document.getElementById("popper-button").style.visibility = "visible";
+      confetti();
+      matches++;
+      wins++;
+      localStorage.setItem('wins', wins);
+      localStorage.setItem('matches', matches);
+      loadStats();
+      return undefined;
+  }
+  if(rowCount == 5){
+    console.log('match complete');
+    matches++;
+    localStorage.setItem('matches', matches);
+    loadStats();
+  }
 }
 
-function alert(text){
-    const alert = document.querySelector(".alert");
-    alert.innerText = text;
-    alert.style.animation = 'none';
-    void alert.offsetWidth; 
-    alert.style.animation = 'fadeInDown 1s ease'; 
-    alert.style.opacity = 100;
-    alert.style.transform = 'translateY(0)';
-    void alert.offsetWidth; 
-    setTimeout(() => { alert.style.animation = 'fadeInUp 1s ease'; }, 1000);
-    alert.style.opacity = 0;
+function loadStats(){
+  document.getElementById('winDisplay').innerText = "Wins: " + wins;
+  document.getElementById('matchDisplay').innerText = "Matches: " + matches;
+  let percent = Math.round(wins/matches * 100);
+  if(percent != NaN){
+    document.getElementById('percentDisplay').innerText = "Victory Percent: " + percent + "%";
+  }
+}
+
+
+function refresh(){
+    location.reload();
+}
+
+function openModal(id){
+    let modal = document.getElementById(id);
+    modal.showModal();
+}
+
+function closeModal(id){
+    let modal = document.getElementById(id);
+    modal.close();
+}
+
+function share(where) {
+  if(where == "native"){
+    let textToCopyOrShare = "Come try this new game!: " + " https://wordpyra.com";
+  }
+  navigator.clipboard.writeText(textToCopyOrShare)
+}
+
+function confetti() {
+  const canvas = document.querySelector("body");
+  const jsConfetti = new JSConfetti()
+  jsConfetti.addConfetti()
 }
